781e8dce1717759c443cfcd629274c7fc17dc5c2
Oxyps <abel_castro_junior@hotmail.com>
2020-10-14 22:33:50 -0300

    fixed products attributes names

--- product/models.py
+++ product/models.py
@@ -5,9 +5,9 @@ class Product(models.Model):
 		db_table = 'product'
 
 	def __str__(self):
-		return f'{self.name} R$ {self.price}'
+		return f'{self.nome} R$ {self.preco}'
 
-	name = models.CharField(unique=True, null=False, max_length=30)
-	description = models.CharField(null=False, max_length=254)
-	price = models.DecimalField(null=False, max_digits=10, decimal_places=2)
+	nome = models.CharField(unique=True, null=False, max_length=30)
+	descricao = models.CharField(null=False, max_length=254)
+	preco = models.DecimalField(null=False, max_digits=10, decimal_places=2)
 	image = models.ImageField(null=False, upload_to='products', max_length=254)
