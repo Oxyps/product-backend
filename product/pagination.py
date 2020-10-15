ef9997bf1702af93ca21451865136950964af7f3
Oxyps <abel_castro_junior@hotmail.com>
2020-10-15 02:15:34 -0300

    fixed prev and next pages format

--- product/pagination.py
+++ product/pagination.py
@@ -2,16 +2,24 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
-    page_size = 10
-    page_query_param= 'page'
+	page_size = 10
+	page_query_param= 'page'
 
-    def get_paginated_response(self, data):
-        return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'prev': self.get_previous_link()
-            },
-            'total': self.page.paginator.count,
-            'page_size': self.page_size,
-            'results': data
-        })
+	def get_next_link(self):
+		if not self.page.has_next():
+			return None
+		return self.page.next_page_number()
+		
+	def get_previous_link(self):
+		if not self.page.has_previous():
+			return None
+		return self.page.previous_page_number()
+
+	def get_paginated_response(self, data):
+		return Response({
+			'next_page': self.get_next_link(),
+			'prev_page': self.get_previous_link(),
+			'page_size': self.page_size,
+			'count_results': self.page.paginator.count,
+			'results': data
+		})
