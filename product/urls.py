de53c2a3b53a2ce075c56f2c15d2e3b6a8461d16
Oxyps <abel_castro_junior@hotmail.com>
2020-10-14 21:14:31 -0300

    added product details

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProductView
+from .views import ProductView, ProductViewDetail
 
 urlpatterns = [
-	path('product/', ProductView.as_view(), name='product'),
+	path('products/', ProductView.as_view(), name='products'),
+	path('products/<int:product_id>/', ProductViewDetail.as_view(), name='product-detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -22,23 +22,30 @@ class ProductView(APIView):
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# class ProductViewDetail(APIView):
-# 	def get(self, request, product_id):
-# 		try:
-#             product = Product.objects.get(pk=product_id)
-#         except Product.DoesNotExist:
-#             raise Http404
-        
-# 		serializer = serializers.ProductSerializer(data=product)
-#         return Response(serializer.data)
+class ProductViewDetail(APIView):
+	def get_object(self, product_id):
+		try:
+			return Product.objects.get(pk=product_id)
+		except Product.DoesNotExist:
+			raise Http404
 
-	# def update(self, request):
+	def get(self, request, product_id):
+		product = self.get_object(product_id)
+		serializer = ProductSerializer(product)
+		return Response(serializer.data, status=status.HTTP_200_OK)
 
-	# def delete(self, request, product_id):
-	#     try:
-	#         restaurant = Restaurant.objects.get(pk=restaurant_id)
-	#     except Restaurant.DoesNotExist:
-	#         raise Http404
-	#     restaurant.delete()
-	#     return Response(status=status.HTTP_204_NO_CONTENT)
+	def put(self, request, product_id):
+		product = self.get_object(product_id)
+		serializer = ProductSerializer(product, data=request.data)
+
+		if serializer.is_valid():
+			serializer.save()
+			return Response(status=status.HTTP_204_NO_CONTENT)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+	def delete(self, request, product_id):
+		product = self.get_object(product_id)
+		product.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
 
