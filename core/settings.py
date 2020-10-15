fcd76507a56f97aa8c833a30fc4325677b9358b8
Oxyps <abel_castro_junior@hotmail.com>
2020-10-15 20:47:53 -0300

    fixed cors settings

--- core/settings.py
+++ core/settings.py
@@ -45,19 +45,22 @@ INSTALLED_APPS = [
 	'product'
 ]
 
-CORS_ORIGIN_WHITELIST = 'http://localhost:3000',
+CORS_ORIGIN_WHITELIST = [
+	'http://localhost:3000',
+	'http://127.0.0.1:3000'
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    
+	'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-	
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
