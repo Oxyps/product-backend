e537a30012d59b6ffa09186243442a4e9f948a87
Oxyps <abel_castro_junior@hotmail.com>
2020-10-19 05:47:34 -0300

    fixed some numbers

--- product/models.py
+++ product/models.py
@@ -9,5 +9,5 @@ class Product(models.Model):
 
 	nome = models.CharField(unique=True, null=False, blank=False, max_length=30)
 	descricao = models.CharField(null=False, blank=False, max_length=254)
-	preco = models.DecimalField(null=False, blank=False, max_digits=10, decimal_places=2)
+	preco = models.DecimalField(null=False, blank=False, max_digits=15, decimal_places=2)
 	# image_url = models.ImageField(null=False, blank=False, upload_to='products', max_length=254)
--- product/pagination.py
+++ product/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
-	page_size = 10
+	page_size = 5
 	page_query_param= 'page'
 
 	def get_next_link(self):
